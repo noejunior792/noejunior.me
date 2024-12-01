da58b7fa9224913b11cfe7e20287ed9140571c8e
noejunior792 <noedombaxe@gmail.com>
2024-12-01 15:34:46 +0100

    add provaaki to the projects area

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,5 +1,12 @@
 export const projects = [
   {
+    title: "Provaaki",
+    description: "Uma plataforma open source para partilha de testes e exames!.",
+    technologies: ["Next.jS", "Javascript", "Tailwind"],
+    link: "https://github.com/noejunior792/provaaki",
+    image: "/images/projects/provaaki.jpg"
+  },
+  {
     title: "GlobalSwapAngola",
     description: "Template feito para uma empresa com foco em importação de produtos e comércio interancional.",
     technologies: ["Vue.js", "Javascript", "Tailwind"],
