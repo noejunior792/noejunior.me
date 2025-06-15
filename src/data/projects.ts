64ce6ec872b3bebf84f0693c3de72a528eaa171f
noejunior792 <noedombaxe@gmail.com>
2025-06-15 19:27:00 +0100

    feat: mudando o footer da pagina2

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,16 +6,17 @@ export const projects = [
     technologies: ["Next.js", "Javascript", "Tailwind"],
     link: "/images/projects/provaaki.jpg",
     url: "",
-    status: "canceled"
+    status: "canceled",
   },
   {
     type: "projeto",
     title: "GlobalSwap",
-    description: "Template feito para uma empresa com foco em importação de produtos e comércio internacional.",
+    description:
+      "Template feito para uma empresa com foco em importação de produtos e comércio internacional.",
     technologies: ["Vue.js", "Javascript", "Tailwind"],
     link: "/images/projects/globalswap.png",
     url: "https://swapp-angola-template.vercel.app/",
-    status: "online"
+    status: "online",
   },
   {
     type: "empresa",
@@ -24,25 +25,37 @@ export const projects = [
     technologies: ["Kotlin", "Android", "Jetpack"],
     link: "https://www.itau.com.br",
     logo: "/images/companies/itau.jpg",
-    period: "2024-Atualmente"
+    period: "2024-Atualmente",
   },
   {
     type: "empresa",
     title: "SuperGenius",
-    description: "A Super Genius é uma startup dedicada à educação em robótica sustentável onde trabalho com CTO.",
+    description:
+      "A Super Genius é uma startup dedicada à educação em robótica sustentável onde trabalho com CTO.",
     technologies: ["Liderança", "Metodologias Ágeis"],
     link: "https://super-genius-rho.vercel.app/",
     logo: "/images/companies/super.webp",
-    period: "2022-Atualmente"
+    period: "2022-Atualmente",
   },
   {
     type: "empresa",
     title: "Techify",
-    description: "Empresa focada em desenvolvimento de software onde trabalho como CEO.",
+    description:
+      "Empresa focada em desenvolvimento de software onde trabalho como CEO.",
     technologies: ["Liderança", "Metodologias Ágeis"],
     logo: "/images/companies/techify.png",
     link: "https://techify.ao",
-        period: "2025 - Atualmente"
+    period: "2025 - Atualmente",
+  },
+  {
+    type: "empresa",
+    title: "CDCI",
+    description:
+      "Empresa focada em desenvolvimento de software onde trabalho como Desenvolvedor Web Fullstack.",
+    technologies: ["Laravel", "HTML", "CSS", "Javascript"],
+    logo: "/images/companies/cdci.png",
+    link: "https://mucua.ao",
+    period: "2023 - Atualmente",
   },
   {
     type: "projeto",
@@ -51,79 +64,87 @@ export const projects = [
     technologies: ["Laravel", "Javascript", "HTML/CSS"],
     link: "/images/projects/itel.png",
     url: "https://itel.gov.ao",
-    status: "online"
+    status: "online",
   },
   {
     type: "projeto",
     title: "Bumba Aki",
-    description: "Produto da Techify focado em prestadores de serviços e clientes.",
+    description:
+      "Produto da Techify focado em prestadores de serviços e clientes.",
     technologies: ["Next.js", "Fastify"],
     link: "/images/projects/bumba.png",
     url: "https://www.bumba.techify.ao",
-    status: "development"
+    status: "development",
   },
   {
     type: "projeto",
     title: "Lead23",
-    description: "Site para disponibilização de cursos da Huawei em parceria com o ITEL.",
+    description:
+      "Site para disponibilização de cursos da Huawei em parceria com o ITEL.",
     technologies: ["Laravel"],
     link: "/images/projects/huawei.png",
     url: "https://lead23.itel.gov.ao/",
-    status: "online"
+    status: "online",
   },
   {
     type: "projeto",
     title: "Ex-Alunos",
-    description: "Uma plataforma digital, dedicada na aproximação de alunos e principalemnte Ex - alunos ou alunos diplomados do ITEL por meio de eventos de inovação tecnológica.",
+    description:
+      "Uma plataforma digital, dedicada na aproximação de alunos e principalemnte Ex - alunos ou alunos diplomados do ITEL por meio de eventos de inovação tecnológica.",
     technologies: ["Laravel"],
     link: "/images/projects/exalunos.png",
     url: "https://ex-alunos.itel.gov.ao/",
-    status: "online"
+    status: "online",
   },
   {
     type: "projeto",
     title: "NoteMark",
-    description: "Um aplicativo de anotações para desktop com suporte a markdown pronto para uso. Desenvolvido com Electron e React.",
+    description:
+      "Um aplicativo de anotações para desktop com suporte a markdown pronto para uso. Desenvolvido com Electron e React.",
     technologies: ["Electron", "React"],
     link: "/images/projects/notemark.png",
     url: "https://github.com/noejunior792/noteMark",
-    status: "online"
+    status: "online",
   },
   {
     type: "projeto",
     title: "Sung",
-    description: "Um aplicativo de chatbot de código aberto desenvolvido com Dart e Flutter, que aproveita o poder da API Gemini para proporcionar conversas inteligentes e interativas.",
+    description:
+      "Um aplicativo de chatbot de código aberto desenvolvido com Dart e Flutter, que aproveita o poder da API Gemini para proporcionar conversas inteligentes e interativas.",
     technologies: ["Flutter", "Dart"],
     link: "/images/projects/sung.png",
     url: "https://github.com/noejunior792/sung",
-    status: "online"
+    status: "online",
   },
   {
     type: "projeto",
     title: "Itel Vagas",
-    description: "Site de Vagas para Alunos do ITEL e publicação de vagas das empresas parceiras.",
+    description:
+      "Site de Vagas para Alunos do ITEL e publicação de vagas das empresas parceiras.",
     technologies: ["Laravel"],
     link: "/images/projects/itelvaga.png",
     url: "https://www.vagas.itel.gov.ao/",
-    status: "online"
+    status: "online",
   },
   {
     type: "projeto",
     title: "Github Profile Analyzer",
-    description: "O GitHub Profile Analyzer é uma aplicação web que permite analisar perfis do GitHub, visualizando estatísticas de usuários, repositórios e linguagens utilizadas. ",
+    description:
+      "O GitHub Profile Analyzer é uma aplicação web que permite analisar perfis do GitHub, visualizando estatísticas de usuários, repositórios e linguagens utilizadas. ",
     technologies: ["Next.js"],
     link: "/images/projects/githubprofile.png",
     url: "https://github-profile-analyzer-alpha.vercel.app/",
-    status: "online"
+    status: "online",
   },
   {
     type: "projeto",
     title: "JSON Share",
-    description: "Uma aplicação web que permite aos usuários compartilhar dados JSON publicamente.",
+    description:
+      "Uma aplicação web que permite aos usuários compartilhar dados JSON publicamente.",
     technologies: ["Next.js", "Clerk", "Shadcn/ui"],
     link: "/images/projects/jsonshare.png",
     url: "https://github.com/noejunior792/json-share",
-    status: "online"
+    status: "online",
   },
   {
     type: "projeto",
@@ -132,25 +153,27 @@ export const projects = [
     technologies: ["Next.js", "Shadcn/ui"],
     link: "/images/projects/horacio.png",
     url: "https://horacio-manuel.vercel.app/",
-    status: "canceled"
+    status: "canceled",
   },
   {
     type: "projeto",
     title: "Layout Animado e Responsivo",
-    description: "É um projeto de estudo focado na criação de um layout animado e responsivo.",
+    description:
+      "É um projeto de estudo focado na criação de um layout animado e responsivo.",
     technologies: ["Flutter", "Dart"],
     link: "/images/projects/responsivescreen.png",
     url: "https://github.com/noejunior792/animatedResponsiveLayout",
-    status: "online"
+    status: "online",
   },
   {
     type: "projeto",
     title: "Kotlin Intellisense",
-    description: "Uma extensão para o Visual Studio Code para melhorar o LSP do Kotlin.",
+    description:
+      "Uma extensão para o Visual Studio Code para melhorar o LSP do Kotlin.",
     technologies: ["Kotlin", "Typescript"],
     link: "/images/projects/kotlinintellense.png",
     url: "https://github.com/noejunior792/kotlinIntellisense",
-    status: "online"
+    status: "online",
   },
   {
     type: "projeto",
@@ -159,15 +182,25 @@ export const projects = [
     technologies: ["HTML", "CSS", "Javascript"],
     link: "/images/projects/pokedex.png",
     url: "https://noejunior792.github.io/pokedex/",
-    status: "online"
+    status: "online",
   },
   {
     type: "projeto",
     title: "Tiny Swords",
-    description: "Um jogo desenvolvido por mim, onde a lei é ´Sobreviva o máximo possível´.",
-    technologies: ["Godot","GodotScript"],
+    description:
+      "Um jogo desenvolvido por mim, onde a lei é ´Sobreviva o máximo possível´.",
+    technologies: ["Godot", "GodotScript"],
     link: "/images/projects/tinyswords.png",
     url: "https://github.com/noejunior792/tiny-swords",
-    status: "online"
+    status: "online",
+  },
+  {
+    type: "projeto",
+    title: "Njila",
+    description: "Aplicativo aconselhador de ações jurídicas.",
+    technologies: ["React Native"],
+    link: "/images/projects/njila.png",
+    url: "https://njila.ao",
+    status: "online",
   },
 ];
