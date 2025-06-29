1694513b58cf7524cec44f80dc96996ccb50309e
noejunior792 <noedombaxe@gmail.com>
2025-06-29 01:53:02 +0100

    fix: Importa @astrojs/node corretamente no astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [
